9fc863f314b6e55f5a596c5f1a2494400895a03a
mosmn <ithinkisee8@gmail.com>
2024-02-11 15:16:20 +0800

    Add authController with register and login functions

---
+++ controllers/authController.js
@@ -0,0 +1,32 @@
+const asyncHandler = require("express-async-handler");
+const User = require("../models/user");
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
+
+exports.register = asyncHandler(async (req, res) => {
+    const { username, email, password, type } = req.body;
+    const user = await User.create({ username, password, email, type });
+    res.status(201).json(user);
+    }
+);
+
+exports.login = async (req, res) => {
+    const { username, password } = req.body;
+
+    const user = await User.findOne({ username });
+
+    if (!user) {
+        return res.status(400).json({ message: 'Invalid username or password' });
+    }
+
+    const isMatch = await bcrypt.compare(password, user.password);
+
+    if (!isMatch) {
+        return res.status(400).json({ message: 'Invalid username or password' });
+    }
+
+    const payload = { id: user.id, username: user.username };
+    const token = jwt.sign(payload, process.env.JWT_SECRET, { expiresIn: '24h' });
+
+    res.json({ token });
+};
