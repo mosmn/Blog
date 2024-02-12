961218885bf280b11675c202c0a9aa04559e4698
mosmn <ithinkisee8@gmail.com>
2024-02-12 11:29:04 +0800

    Refactor passport.js to use single quotes and improve code formatting

--- config/passport.js
+++ config/passport.js
@@ -1,23 +1,23 @@
-const JwtStrategy = require('passport-jwt').Strategy;
-const ExtractJwt = require('passport-jwt').ExtractJwt;
+const JwtStrategy = require("passport-jwt").Strategy;
+const ExtractJwt = require("passport-jwt").ExtractJwt;
 const User = require("../models/user");
 
 const options = {
-    jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
-    secretOrKey: process.env.JWT_SECRET
+  jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+  secretOrKey: process.env.JWT_SECRET,
 };
 
-module.exports = passport => {
-    passport.use(
-        new JwtStrategy(options, (jwt_payload, done) => {
-            User.findById(jwt_payload.id)
-                .then(user => {
-                    if (user) {
-                        return done(null, user);
-                    }
-                    return done(null, false);
-                })
-                .catch(err => console.log(err));
+module.exports = (passport) => {
+  passport.use(
+    new JwtStrategy(options, (jwt_payload, done) => {
+      User.findById(jwt_payload.id)
+        .then((user) => {
+          if (user) {
+            return done(null, user);
+          }
+          return done(null, false);
         })
-    );
+        .catch((err) => console.log(err));
+    }),
+  );
 };
