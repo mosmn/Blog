d0f250eb6180ee3e0410f4e4222e1be1ebbf28f7
mosmn <ithinkisee8@gmail.com>
2024-02-12 09:27:47 +0800

    Refactor app.js file structure and update dependencies

--- app.js
+++ app.js
@@ -1,72 +1,57 @@
 /* eslint-disable new-cap */
+/* eslint-disable require-jsdoc */
 const express = require("express");
 const path = require("path");
 const cookieParser = require("cookie-parser");
 const session = require("express-session");
-const passport = require("./config/passport");
 const logger = require("morgan");
-
-require("dotenv").config();
-
-// const indexRouter = require("./routes/index");
-// const catalogRouter = require("./routes/catalog");
-// const auth = require("./routes/auth");
-
-// app.use("/", indexRouter);
-// app.use("/catalog", catalogRouter);
-// app.use("/catalog/auth", auth);
-
+const passport = require('passport');
 const compression = require("compression");
 const helmet = require("helmet");
+const RateLimit = require("express-rate-limit");
+const mongoose = require("mongoose");
+require("dotenv").config();
+
+const authRoutes = require('./routes/auth');
+const blogRoutes = require('./routes/blog');
 
 const app = express();
-
-app.use(session({ secret: 'cats', resave: false, saveUninitialized: true }));
-
-app.use(passport.initialize());
-app.use(passport.session());
-
-app.use(
-  helmet.contentSecurityPolicy({
-    directives: {
-      "script-src": ["'self'", "code.jquery.com", "cdn.jsdelivr.net"],
-    },
-  }),
-);
-
-const RateLimit = require("express-rate-limit");
-const limiter = RateLimit({
-  windowMs: 1 * 60 * 1000, // 1 minute
-  max: 20,
-});
-
-app.use(limiter);
-
-const mongoose = require("mongoose");
-mongoose.set("strictQuery", false);
 const mongoDB = process.env.MONGODB_URI;
 
-main().catch((err) => console.log(err));
-// eslint-disable-next-line require-jsdoc
-async function main() {
-  await mongoose.connect(mongoDB);
-  console.log("Connected to MongoDB");
-}
-
-app.use(logger("dev"));
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
-
+app.use(session({ secret: "cats", resave: false, saveUninitialized: true }));
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(logger("dev"));
 app.use(compression());
-
 app.use(express.static(path.join(__dirname, "public")));
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
-}
+app.use(
+    helmet.contentSecurityPolicy({
+        directives: {
+            "script-src": ["'self'", "code.jquery.com", "cdn.jsdelivr.net"],
+        },
+    }),
 );
 
+const limiter = RateLimit({
+    windowMs: 1 * 60 * 1000, // 1 minute
+    max: 20,
+});
+app.use(limiter);
+
+async function main() {
+    await mongoose.connect(mongoDB);
+    console.log("Connected to MongoDB");
+}
+mongoose.set("strictQuery", false);
+main().catch((err) => console.log(err));
+
+app.use('/auth', authRoutes);
+app.use('/blog', blogRoutes);
+
 app.listen(process.env.PORT, () =>
-  console.log(`Example app listening on port ${process.env.PORT}!`),
+    console.log(`Example app listening on port ${process.env.PORT}!`),
 );
